bb0a1ad13a8e52fc5ad5f27f01d96133d3309702
Donovan An <donovantaean@yahoo.com>
2017-01-10 15:20:30 -0800

    finished 10

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
@@ -59,28 +60,74 @@ namespace ConsoleApplication
         }        
 
 
+        // Array with Odd Numbers
+        public static void oddArray(){
+            List<int> oddList = new List<int>();
+            for(int i = 1; i <=255; i+=2){
+                oddList.Add(i);
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", oddList));
+        }    
+
+
+        // Greater than Y
+        public static void greaterThanY(int[] arr, int Y){
+            int count = 0;
+            for(int i = 0; i < arr.Length; i++){
+                if(arr[i] > Y){
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }        
+        
+
+        // Square the Values
+        public static void square(int[] arr){
+            for(int i = 0; i < arr.Length; i++){
+                arr[i] = arr[i]*arr[i];
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", arr));
+        }
+
+
+        // Eliminate Negative Numbers
+        public static void zeroNegatives(int[] arr){
+            for(int i = 0; i < arr.Length; i++){
+                if(arr[i] < 0){
+                    arr[i] = 0;
+                }
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", arr));
+        }
+        
+        
+
+
+
+
 
 
         
         
         
-        
-        
-        
-        
-        
+
         
         
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[] myArray = {1,3,52,7,9,13};
+            int[] myArray = {1,3,-52,-7,9,13};
             // print255();
             // printOdds();
             // printSums();
             // printArray(myArray);
             // findMax(myArray);
-            average(myArray);
+            // average(myArray);
+            // oddArray();
+            // greaterThanY(myArray, 5);
+            // square(myArray);
+            zeroNegatives(myArray);
         }
     }
 }
