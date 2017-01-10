48e5ed774a91d66f015c1ebd632286049ba7355a
Donovan An <donovantaean@yahoo.com>
2017-01-10 14:44:19 -0800

    completed challenge 4

--- Program.cs
+++ Program.cs
@@ -4,9 +4,61 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        // Print 1-255
+        public static void print255(){
+            for(int i = 1; i <=255; i++){
+                Console.WriteLine(i);
+            }
+        }
+
+
+        // Print odd numbers between 1-255
+        public static void printOdds(){
+            for(int i = 1; i <=255; i+=2){
+                Console.WriteLine(i);
+            }
+        }
+
+        // Print Sum
+        public static void printSums(){
+            int sum = 0;
+            for(int i = 1; i <=255; i++){
+                sum += i;
+                Console.WriteLine("New Number: " + i + " - New Sum: " + sum);
+            }
+
+        }
+        
+        // Iterating through an Array
+        public static void printArray(int[] arr){
+            for( int i = 0; i < arr.Length; i++){
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] myArray = {1,3,5,7,9,13};
+            // print255();
+            // printOdds();
+            // printSums();
+            printArray(myArray);
+
         }
     }
 }
